cb9456323ba077fd9a4cf412e0866252d5b08357
unknown <kyogeshsagar100@gmail.com>
2026-06-15 20:29:44 +0530

    Move MongoDB URI to environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://kyogeshsagar100_db_user:KGOpalRao@cluster0.vrl3bpc.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-
+export const connectDB = async () => {
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("DB Connected"));
 }
